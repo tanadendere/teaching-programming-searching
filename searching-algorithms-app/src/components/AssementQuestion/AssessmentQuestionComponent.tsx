831d8e1ef680dce4907e6556e75d454b474567e8
Jonah Gasura <JonahG@assupol.co.za>
2023-11-07 22:57:32 +0200

    Added scores to Quiz and Assessment components

--- searching-algorithms-app/src/components/AssementQuestion/AssessmentQuestionComponent.tsx
+++ searching-algorithms-app/src/components/AssementQuestion/AssessmentQuestionComponent.tsx
@@ -5,15 +5,12 @@ import { Button, Dialog, DialogActions, DialogContent, DialogContentText, Dialog
 
 import styles from '../../styles/quiz.module.css'
 
-export default function AssessmentQuestionComponent({questionNumber, questionText, answerOne, answerTwo, answerThree, answerFour, correctAnswerNumber, isFinalAssessment}: AssessmentQuestionDetails) {
+export default function AssessmentQuestionComponent({questionNumber, questionText, answerOne, answerTwo, answerThree, answerFour, correctAnswerNumber, isFinalAssessment, onAnswerClick}: AssessmentQuestionDetails) {
 
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [openFeedback, setOpenFeedback] = useState(false);
 
     const determineButtonClass = (answerNumber: any) => {
-        if (selectedAnswer === answerNumber && selectedAnswer !== correctAnswerNumber) {
-            return styles.wrongAnswer;
-        }
         if (selectedAnswer === answerNumber) {
             return styles.selectedAnswer;
         }
@@ -23,11 +20,13 @@ export default function AssessmentQuestionComponent({questionNumber, questionTex
     const handleAnswerClick = (answerNumber: any) => {
         setSelectedAnswer(answerNumber);
         setOpenFeedback(true);
+
+        onAnswerClick(answerNumber, correctAnswerNumber);
     };
 
     const handleClose = () => {
         setOpenFeedback(false);
-        setSelectedAnswer(null); 
+        setSelectedAnswer(null);
     };
 
     return (
@@ -38,56 +37,52 @@ export default function AssessmentQuestionComponent({questionNumber, questionTex
             </div>
 
             <div className={styles.answerCard}>
-                <button
-                    className={`${styles.answerButton} ${determineButtonClass(1)}`}
-                    onClick={() => handleAnswerClick(1)}
-                >
+                <button className={`${styles.answerButton} ${determineButtonClass(1)}`} onClick={() => handleAnswerClick(1)}>
                     {answerOne}
                 </button>
-                <button
-                    className={`${styles.answerButton} ${determineButtonClass(2)}`}
-                    onClick={() => handleAnswerClick(2)}
-                >
+                <button className={`${styles.answerButton} ${determineButtonClass(2)}`} onClick={() => handleAnswerClick(2)}>
                     {answerTwo}
                 </button>
             </div>
 
             <div className={styles.answerCard}>
-                <button
-                    className={`${styles.answerButton} ${determineButtonClass(3)}`}
-                    onClick={() => handleAnswerClick(3)}
-                >
+                <button className={`${styles.answerButton} ${determineButtonClass(3)}`} onClick={() => handleAnswerClick(3)}>
                     {answerThree}
                 </button>
-                <button
-                    className={`${styles.answerButton} ${determineButtonClass(4)}`}
-                    onClick={() => handleAnswerClick(4)}
-                >
+                <button className={`${styles.answerButton} ${determineButtonClass(4)}`} onClick={() => handleAnswerClick(4)}>
                     {answerFour}
                 </button>
             </div>
 
-            {!isFinalAssessment && <Dialog open={openFeedback} onClose={handleClose}>
-                <DialogTitle>
-                    {selectedAnswer === correctAnswerNumber &&
-                        <div style={{ color: 'green' }}><strong>Correct Answer!</strong></div>}
-                    {selectedAnswer !== correctAnswerNumber &&
-                        <div style={{ color: 'red' }}><strong>Wrong Answer. Try Again!</strong></div>}
-                </DialogTitle>
+            {!isFinalAssessment && (
+                <Dialog open={openFeedback} onClose={handleClose}>
+                    <DialogTitle>
+                        {selectedAnswer === correctAnswerNumber && (
+                            <div style={{ color: 'green' }}>
+                                <strong>Correct Answer!</strong>
+                            </div>
+                        )}
+                        {selectedAnswer !== correctAnswerNumber && (
+                            <div style={{ color: 'red' }}>
+                                <strong>Wrong Answer. Try Again!</strong>
+                            </div>
+                        )}
+                    </DialogTitle>
 
-                <DialogContent>
-                    <DialogContentText>
-                        {selectedAnswer === correctAnswerNumber
-                            ? 'Well done! You selected the correct answer.'
-                            : 'Oops! You selected the wrong answer. Please try again.'}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} variant="contained" color="primary">
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>}
+                    <DialogContent>
+                        <DialogContentText>
+                            {selectedAnswer === correctAnswerNumber
+                                ? 'Well done! You selected the correct answer.'
+                                : 'Oops! You selected the wrong answer. Please try again.'}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} variant="contained" color="primary">
+                            Close
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            )}
         </div>
     );
 
